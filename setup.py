6ffc73727a40b5dc0cd1ec7c28f0425cd087c8bb
johodges <johodges@vt.edu>
2023-09-08 09:48:49 -0400

    increment setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pyfdstools',
-    version='0.0.12',    
+    version='0.0.13',    
     description='This software is part of a python library to assist in developing and analyzing simulation results from Fire Dynamics Simulator (FDS). FDS is an open source computational fluid dynamics (CFD) software package developed by NIST. The sourcecode is available at: https://github.com/firemodels/fds',
     url='https://github.com/johodges/pyfdstools',
     author='Jonathan Hodges',

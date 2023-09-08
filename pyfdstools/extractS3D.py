ea966186c44ea6538d83a8b3a36d2a69f14e00a1
johodges <johodges@vt.edu>
2023-09-08 08:58:10 -0400

    expand s3d functionality

--- pyfdstools/extractS3D.py
+++ pyfdstools/extractS3D.py
@@ -87,7 +87,7 @@ def readS3dFile(file):
     ordered_data = np.array(ordered_data, dtype=np.uint8)
     return times, ordered_data
 
-def extractS3dValues(resultDir, chid):
+def extractS3dValues(resultDir, chid, decode=True):
     values = defaultdict(bool)
     s3dfiles = getFileList(resultDir, chid, 's3d')
     smvFile = getFileList(resultDir, chid, 'smv')[0]
@@ -102,24 +102,27 @@ def extractS3dValues(resultDir, chid):
         quantity = files['SMOKF3D'][file]['QUANTITY']
         f_ordered_data = np.array(ordered_data, dtype=np.float32)
         meshNum = int(files['SMOKF3D'][file]['LINETEXT'].split()[1])-1
-        if quantity == 'SOOT DENSITY':
-            grid = grids[meshNum]
-            dx = np.median(grid[0][1:,1]-grid[0][:-1,1])
-            dy = np.median(grid[1][1:,1]-grid[1][:-1,1])
-            dz = np.median(grid[2][1:,1]-grid[2][:-1,1])
-            dd = (dx*dy*dz)**(1/3)
-            MAX_SMV = 8700
-            MASS_EXTINCTION_COEFFICIENT = MAX_SMV
-            factor = -1*(MASS_EXTINCTION_COEFFICIENT) * dd
-            val_fds = np.log(-1*((f_ordered_data / 254) - 1.0))/factor
-        if quantity == 'HRRPUV':
-            MAX_SMV = 1200
-            val_fds = (f_ordered_data/254)*MAX_SMV
-        if quantity == 'TEMPERATURE':
-            MAX_SMV = 2000
-            TMPA = 20 + 273.15
-            TMPM = 273.15
-            val_fds = (f_ordered_data/254)*(MAX_SMV-(TMPA-TMPM)) + (TMPA-TMPM)
+        if decode:
+            if quantity == 'SOOT DENSITY':
+                grid = grids[meshNum]
+                dx = np.median(grid[0][1:,1]-grid[0][:-1,1])
+                dy = np.median(grid[1][1:,1]-grid[1][:-1,1])
+                dz = np.median(grid[2][1:,1]-grid[2][:-1,1])
+                dd = (dx*dy*dz)**(1/3)
+                MAX_SMV = 8700
+                MASS_EXTINCTION_COEFFICIENT = MAX_SMV
+                factor = -1*(MASS_EXTINCTION_COEFFICIENT) * dd
+                val_fds = np.log(-1*((f_ordered_data / 254) - 1.0))/factor
+            if quantity == 'HRRPUV':
+                MAX_SMV = 1200
+                val_fds = (f_ordered_data/254)*MAX_SMV
+            if quantity == 'TEMPERATURE':
+                MAX_SMV = 2000
+                TMPA = 20 + 273.15
+                TMPM = 273.15
+                val_fds = (f_ordered_data/254)*(MAX_SMV-(TMPA-TMPM)) + (TMPA-TMPM)
+        else:
+            val_fds = f_ordered_data
         if values[meshNum] is False:
             values[meshNum] = defaultdict(bool)
         values[meshNum][quantity] = val_fds
@@ -136,13 +139,13 @@ def encodeS3dData(data, quantity, dx):
         encoded_data = np.array(np.round((1-np.exp(data*factor))*254), dtype=np.uint8)
     if quantity == 'HRRPUV':
         MAX_SMV = 1200
-        encoded_data = np.array((data/MAX_SMV)*254, dtype=np.uint8)
+        encoded_data = np.array(np.round((data/MAX_SMV)*254), dtype=np.uint8)
     if quantity == 'TEMPERATURE':
         MAX_SMV = 2000
         TMPA = 20 + 273.15
         TMPM = 273.15
         TMP_MIN = TMPA-TMPM
-        encoded_data = np.array(254*(data - TMP_MIN)/(MAX_SMV-TMP_MIN), dtype=np.uint8)
+        encoded_data = np.array(np.round(254*(data - TMP_MIN)/(MAX_SMV-TMP_MIN)), dtype=np.uint8)
     return encoded_data
 
 def writeS3dFile(file, times, data_out, quantity, dx):
@@ -196,3 +199,89 @@ def writeS3dFile(file, times, data_out, quantity, dx):
         f.write(np.array([nchars_out], dtype=int32).tobytes())
         
     f.close()
+    
+def writeS3dFile_debug(file, debug_file, times, data_out, quantity, dx):
+    endianness = "<"
+    float32 = np.dtype(np.float32).newbyteorder(endianness)
+    int32 = np.dtype(np.int32).newbyteorder(endianness)
+    
+    f = zopen(debug_file, 'rb')
+    debug_data = f.read()
+    f.close()
+    
+    f = zopen(file, 'wb')
+    f.write(np.array([32, 1, 0], dtype=int32).tobytes())
+    NT, NX, NY, NZ = data_out.shape
+    f.write(np.array([0, NX-1, 0, NY-1, 0, NZ-1], dtype=int32).tobytes())
+    f.write(np.array([32], dtype=int32).tobytes())
+    
+    nchars_in = NX*NY*NZ
+    encoded_data = encodeS3dData(data_out, quantity, dx)
+    f.close()
+    for i, time in enumerate(times):
+        f = zopen(file, 'ab')
+        f.write(np.array([4], dtype=int32).tobytes())
+        f.write(times[i].tobytes())
+        f.write(np.array([4, 8], dtype=int32).tobytes())
+        f.close()
+        
+        #times[i].tobytes()+np.array([4], dtype=int32).tobytes()+
+        array_data = encoded_data[i, :, :, :].reshape((nchars_in), order='F')
+        encoded_list = encode_list(array_data)
+        
+        # Hack job to calculate number of chars before going through the looping logic
+        count = [x[0] for x in encoded_list]
+        counter = np.sum([x for x in count if x <4])
+        count = [x for x in count if x > 3]
+        counter = counter + np.sum([np.floor(x/254) for x in count])*3
+        count = [x-np.floor(x/254)*254 for x in count]
+        counter = counter + np.sum([x for x in count if x <4])
+        count = [x for x in count if x > 3]
+        nchars_out = counter + len(count)*3
+        
+        f = zopen(file, 'ab')
+        f.write(np.array([nchars_in, nchars_out, 8, nchars_out], dtype=int32).tobytes())
+        f.close()
+        tmp = b""
+        for en in encoded_list:
+            #if en[0] > 254:
+            while en[0] > 254:
+                #tmp = tmp + b'\xff' + en[1].tobytes() + b'\xfe'
+                f = zopen(file, 'ab')
+                f.write(b'\xff' + en[1].tobytes() + b'\xfe')
+                f.close()
+                f = zopen(file, 'rb')
+                data = f.read()
+                f.close()
+                l = len(data)
+                if debug_data[:l] != data:
+                    assert False, "Stopped"
+                en[0] = en[0]-254
+            if en[0] > 3:
+                #tmp = tmp + b'\xff' + en[1].tobytes() + np.uint8(en[0]).tobytes()
+                f = zopen(file, 'ab')
+                f.write(b'\xff' + en[1].tobytes() + np.uint8(en[0]).tobytes())
+                f.close()
+                f = zopen(file, 'rb')
+                data = f.read()
+                f.close()
+                l = len(data)
+                if debug_data[:l] != data:
+                    assert False, "Stopped"
+                en[0] = 0
+            while en[0] > 0:
+                #tmp = tmp + en[1].tobytes()
+                f = zopen(file, 'ab')
+                f.write(en[1].tobytes())
+                f.close()
+                f = zopen(file, 'rb')
+                data = f.read()
+                f.close()
+                l = len(data)
+                if debug_data[:l] != data:
+                    assert False, "Stopped"
+                en[0] = en[0] - 1
+        f = zopen(file, 'ab')
+        f.write(np.array([nchars_out], dtype=int32).tobytes())
+        f.close()
+    
